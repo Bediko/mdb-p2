a05d32c866b812ba947a725cba6cbee708181f14
Bediko <bediko@chaosdorf.de>
2014-05-28 15:42:23 +0200

    First try for A_k, not really working

--- mdb2.cpp
+++ mdb2.cpp
@@ -29,72 +29,95 @@ const int VBIN_SIZE = VMAX / VBINS;
 using namespace cv;
 using namespace std;
 
-list<Mat> readImagesFromDirectory(string dirpath = "bilder") {
-	cout<<"huh"<<endl;
-	list<Mat> imagelist;
-	list<string> filenames;	
-	// create directory object and open it
-	tinydir_dir dir;
-	tinydir_open(&dir, dirpath.c_str());
+list<Mat> readImagesFromDirectory(string dirpath = "bilder")
+{
+    cout << "huh" << endl;
+    list<Mat> imagelist;
+    list<string> filenames;
+    // create directory object and open it
+    tinydir_dir dir;
+    tinydir_open(&dir, dirpath.c_str());
 
-	while (dir.has_next)
-	{
-		// create file
-		tinydir_file file;
-		tinydir_readfile(&dir, &file);
-		
-		
-		// if file isnt a directory and if file isnt the DICOMDIR file
-		if (!file.is_dir)
-		{
-			// parse 
-			filenames.push_back(file.name);
-		}
-		
-		// read next file
-		tinydir_next(&dir);
-	}
-	
-	// close directory object
-	tinydir_close(&dir);
-	
-	// sort filenames
-	filenames.sort();
+    while (dir.has_next)
+    {
+        // create file
+        tinydir_file file;
+        tinydir_readfile(&dir, &file);
 
-	list<string>::iterator iter;
-	int i = 0;
-	for(iter = filenames.begin(); iter != filenames.end(); iter++)
-	{
-		// create image from file
-		Mat img = imread(dirpath+"/"+(*iter), 0);
-		if(img.data)
-		{
-			imagelist.push_back(img);
-			ostringstream os;
-			os << "original " << i << ".png";
-			namedWindow(os.str(), CV_WINDOW_AUTOSIZE);
-			imshow(os.str(), img);
-		}
-		i++;
-	}
 
-	return imagelist;
+        // if file isnt a directory and if file isnt the DICOMDIR file
+        if (!file.is_dir)
+        {
+            // parse
+            filenames.push_back(file.name);
+        }
+
+        // read next file
+        tinydir_next(&dir);
+    }
+
+    // close directory object
+    tinydir_close(&dir);
+
+    // sort filenames
+    filenames.sort();
+
+    list<string>::iterator iter;
+    int i = 0;
+    for (iter = filenames.begin(); iter != filenames.end(); iter++)
+    {
+        // create image from file
+        Mat img = imread(dirpath + "/" + (*iter), 0);
+        if (img.data)
+        {
+            imagelist.push_back(img);
+           /* ostringstream os;
+            os << "original " << i << ".png";
+            namedWindow(os.str(), CV_WINDOW_AUTOSIZE);
+            imshow(os.str(), img);*/
+        }
+        i++;
+    }
+
+    return imagelist;
 }
 
-int main( int argc, char** argv )
+double calculateCoarseness(Mat image)
 {
-	list<Mat> images=readImagesFromDirectory();
-	int i=0;
-	while(!images.empty()){
-		Mat image=images.front();
-		images.pop_front();
-		ostringstream os;
-		os << i;
-		namedWindow(os.str(), CV_WINDOW_AUTOSIZE);
-		imshow(os.str(), image);
-		cout<<i<<endl;
-		i++;
-	}
-	waitKey(0);
+    double a[6] = {0};
+    for (int k = 1; k < 6; k++)
+    {//pow(2,k)/2 should be enough 
+        for (int x = pow(2, k ); x < image.cols-pow(2, k ); x++)
+        {
+            for (int y=pow(2, k ); y < image.rows-pow(2, k ); y++)
+            {
+                for (int i = x - pow(2, k - 1); i < x + pow(2, k - 1)-1; i++)
+                {
+                    for (int j = y - pow(2, k - 1); j < y + pow(2, k - 1)-1; j++)
+                    {
+                        a[k] = ((int)image.at<uchar>(i, j)) / (pow(2, 2 * k));
+                        //cout<<"a["<<k<<"]"<<a[k]<<" i "<<i<<" j "<<j<<" y "<<y<<endl;
+                    }
+                }//cout<<"tur"<<endl;
+                //cout<<"hu"<<endl;
+            }
+        }
+        cout<<k<<endl;
+    }
+}
+
+int main( int argc, char **argv )
+{
+    list<Mat> images = readImagesFromDirectory();
+    int i = 0;
+    double a;
+    while (!images.empty())
+    {
+        Mat image = images.front();
+        images.pop_front();
+        a=calculateCoarseness(image);
+
+    }
+    waitKey(0);
 
 }
